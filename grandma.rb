1969664e83d4c7489f9ce80ecffad07c744be6c1
Lauren Days <lmchambers0420@gmail.com>
2018-11-11 22:24:52 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,6 +18,6 @@ def speak_to_grandma(phrase)
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else 
-    return "HUH?!, SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
