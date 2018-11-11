0909dfe62d4fd3d48cf9f680cbffa5e2000d56ac
Lauren Days <lmchambers0420@gmail.com>
2018-11-11 22:23:38 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,6 +17,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
     elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  else "HUH?!, SPEAK UP, SONNY!"
-end
+  else 
+    return "HUH?!, SPEAK UP, SONNY!"
+  end
 end
